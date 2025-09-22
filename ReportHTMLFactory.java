9411e23299b7921b50c9c325d744559f382c8e55
camilo2212-ui <145920105+camilo2212-ui@users.noreply.github.com>
2025-09-21 23:05:18 -0500

    Update ReportHTMLFactory.java

--- ReportHTMLFactory.java
+++ ReportHTMLFactory.java
@@ -11,27 +11,7 @@ public class ReportHTMLFactory extends ReportFactory {
     }
 }
 
-// Fábrica para Reportes PDF
-public class ReportPDFFactory extends ReportFactory {
-    private boolean encrypted;
 
-    public ReportPDFFactory(boolean encrypted) {
-        this.encrypted = encrypted;
-    }
-
-    @Override
-    public Report createReport(String title, String content) {
-        return new ReportPDF(title, content, encrypted);
-    }
-}
-
-// Fábrica para Reportes Excel
-public class ReportExcelFactory extends ReportFactory {
-    private int sheets;
-
-    public ReportExcelFactory(int sheets) {
-        this.sheets = sheets;
-    }
 
     @Override
     public Report createReport(String title, String content) {
